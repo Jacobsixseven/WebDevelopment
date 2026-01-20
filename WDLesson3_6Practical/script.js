e33cd1945c0b4752f01e0460badaf63f34bc0810
Jacobsixseven <jacobs383@nycstudents.net>
2026-01-20 14:07:53 +0000

    uiu

--- WDLesson3_6Practical/script.js
+++ WDLesson3_6Practical/script.js
@@ -1,7 +1,22 @@
 // Challenge 2:  Create the function for the Average program.  Refer to the resource link in the HTML for assistance.
+function Average(){
+    parseFloat(document.getElementById)("g1").value
+    parseFloat(document.getElementById)("g2").value
+    parseFloat(document.getElementById)("g3").value
 
+    
+}
 
 // Challenge 4:  Create the function for the Slope program.  Refer to the resource link in the HTML for assistance.
-
+function Slope(){
+    parseFloat(document.getElementById)("x1").value
+    parseFloat(document.getElementById)("x2").value
+    parseFloat(document.getElementById)("y1").value
+    parseFloat(document.getElementById)("y2").value
+}
 
 // Challenge 6:  Create the function for the BMI program.  Refer to the resource link in the HTML for assistance.
+function BMI(){
+    parseFloat(document.getElementById)(W).value
+    parseFloat(document.getElementById)(H).value
+}
