02a2f003a23758b3e359bd53601077abdd18c616
Jacobsixseven <jacobs383@nycstudents.net>
2026-02-03 14:16:30 +0000

    h

--- WDLesson3_7Practical/script.js
+++ WDLesson3_7Practical/script.js
@@ -1,8 +1,16 @@
 /* Challenge 2: Create a function to serve as the event handler for the distance UI. Guidelines,
       1) Create variables and retrieve the information from the text inputs you created in Challenge 1.
       2) Perform the necessary calculations
-      3) Display the results in the appropriate element
-*/
+      3) Display the results in the appropriate element\
+*/Distance(){
+      let x1 = parseFloat(document.getElementById("x1").value);
+      let x2 = parseFloat(document.getElementById("x2").value);
+      let y1 = parseFloat(document.getElementById("y1").value);
+      let y2 = parseFloat(document.getElementById("y2").value);
+      let output = document.getElementById("output")
+
+      let first = Math.pow(x2)
+}
 
 /* Challenge 4: Create a function to serve as the event handler for the compound interest UI. Guidelines,
       1) Create variables and retrieve the information from the text inputs you created in Challenge 1.
@@ -10,5 +18,15 @@
       3) Display the results in the appropriate element
 */
 
+Function Intrest(){
+      let P = parseFloat(document.getElementById("p").value)
+}     let R = parseFloat(document.getElementById("p").value)
+      let N = parseFloat(document.getElementById("p").value)
+      let T = parseFloat(document.getElementById("p").value)
 
+      let Paranthesis = 1+ (r /n)
 
+      let Para2 = Math.pow(Paranthesis, N * T)
+
+      let FA = P * Para2
+}
